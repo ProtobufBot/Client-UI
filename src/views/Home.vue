89eba0de880d184eb7c4cec54b002c16b08b2253
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2021-03-31 19:41:15 +0800

    qrcode login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,196 +1,236 @@
 <template>
-    <v-container class="home">
-        <v-card class="pa-4 ma-4">
+  <v-container class="home">
+
+    <v-btn @click="showCreateDialog=true">创建机器人</v-btn>
+    <v-dialog v-model="showCreateDialog">
+      <v-card class="pa-4">
+        <v-row>
+          <v-col cols="6">
             <v-row>
-                <v-col><h2>创建机器人</h2></v-col>
-                <v-col cols="4">
-                    <v-text-field dense v-model="createBotId" outlined placeholder="QQ"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                    <v-text-field dense v-model="createBotPassword" outlined placeholder="密码"
-                                  type="password"></v-text-field>
-                </v-col>
-                <v-col cols="2">
-                    <v-btn @click="createBot" color="primary">创建</v-btn>
-                </v-col>
+              <v-col cols="12">
+                <h2>扫码创建</h2>
+              </v-col>
+              <v-col cols="12">
+                <v-btn @click="onFetchQRCodeClick">获取二维码</v-btn>
+              </v-col>
+              <v-col cols="12">
+                <img style="width:300px;height:300px;background: gray"
+                     v-if="!!this.fetchQRCodeResp.imageData"
+                     :src="getImage(this.fetchQRCodeResp.imageData)">
+              </v-col>
             </v-row>
-
-            <v-container>
-                <h1>机器人列表</h1>
-                <v-data-table :items="listBotResp.botList" :headers="botTableHeaders">
-                    <template v-slot:item.isOnline="{ item }">
-                        <v-chip :color="item.isOnline?'success':'error'">{{item.isOnline?'在线':'离线'}}</v-chip>
-                    </template>
-                    <template v-slot:item.login="{ item }">
-                        <v-btn :disabled="item.isOnline" @click="login(item.botId)" color="success">登陆</v-btn>
-                    </template>
-                </v-data-table>
-            </v-container>
-        </v-card>
-
-
-        <v-card class="pa-4 ma-4">
+          </v-col>
+          <v-col cols="6">
             <v-row>
-                <v-col><h2>处理验证码</h2></v-col>
-                <v-col cols="4">
-                    <v-text-field dense v-model="captchaBotId" outlined placeholder="QQ"></v-text-field>
-                </v-col>
-                <v-col cols="4">
-                    <v-text-field dense v-model="captchaResult" outlined placeholder="结果"></v-text-field>
-                </v-col>
-                <v-col cols="2">
-                    <v-btn @click="solveCaptcha" color="primary">提交</v-btn>
-                </v-col>
+              <v-col cols="12">
+                <h2>使用账号密码</h2>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field dense v-model="createBotId" outlined placeholder="QQ"/>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field dense v-model="createBotPassword" outlined placeholder="密码"
+                              type="password"/>
+              </v-col>
+              <v-col cols="12">
+                <v-btn block @click="createBot" color="primary">创建</v-btn>
+              </v-col>
             </v-row>
-            <v-container>
-                <h1>验证码列表</h1>
-                <v-data-table :items="listCaptchaResp.captchaList" :headers="captchaTableHeaders">
-                    <template v-slot:item.captchaType="{ item }">
-                        <span v-if="item.captchaType===CaptchaType.PIC_CAPTCHA">图片</span>
-                        <span v-if="item.captchaType===CaptchaType.SLIDER_CAPTCHA">滑动</span>
-                        <span v-if="item.captchaType===CaptchaType.UNSAFE_DEVICE_LOGIN_VERIFY">设备锁</span>
-                    </template>
-                    <template v-slot:item.captchaType="{ item }">
-                        <span v-if="item.captchaType===CaptchaType.PIC_CAPTCHA">图片</span>
-                        <span v-if="item.captchaType===CaptchaType.SLIDER_CAPTCHA">滑动</span>
-                        <span v-if="item.captchaType===CaptchaType.UNSAFE_DEVICE_LOGIN_VERIFY">设备锁</span>
-                        <span v-if="item.captchaType===CaptchaType.SMS">短信</span>
-                    </template>
-                    <template v-slot:item.detail="{ item }">
-                        <img v-if="item.captchaType===CaptchaType.PIC_CAPTCHA" :src="getImage(item.image)"
-                             style="height: 36px"/>
-                        <a v-if="item.captchaType===CaptchaType.SLIDER_CAPTCHA" target="_blank" :href="item.url">链接</a>
-                        <a v-if="item.captchaType===CaptchaType.UNSAFE_DEVICE_LOGIN_VERIFY" target="_blank"
-                           :href="item.url">链接</a>
-                        <span v-if="item.captchaType===CaptchaType.SMS">{{item.url}}</span>
-                    </template>
-                </v-data-table>
-
-            </v-container>
-        </v-card>
+          </v-col>
+        </v-row>
 
 
-    </v-container>
+      </v-card>
+    </v-dialog>
+
+    <v-card class="pa-4 ma-4">
+      <v-container>
+        <h1>机器人列表</h1>
+        <v-data-table :items="listBotResp.botList" :headers="botTableHeaders">
+          <template v-slot:item.isOnline="{ item }">
+            <v-chip :color="item.isOnline?'success':'error'">{{ item.isOnline ? '在线' : '离线' }}</v-chip>
+          </template>
+          <template v-slot:item.login="{ item }">
+            <v-btn :disabled="item.isOnline" @click="login(item.botId)" color="success">登陆</v-btn>
+          </template>
+        </v-data-table>
+      </v-container>
+    </v-card>
+
+
+    <v-card class="pa-4 ma-4">
+      <v-row>
+        <v-col><h2>处理验证码</h2></v-col>
+        <v-col cols="4">
+          <v-text-field dense v-model="captchaBotId" outlined placeholder="QQ"></v-text-field>
+        </v-col>
+        <v-col cols="4">
+          <v-text-field dense v-model="captchaResult" outlined placeholder="结果"></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-btn @click="solveCaptcha" color="primary">提交</v-btn>
+        </v-col>
+      </v-row>
+      <v-container>
+        <h1>验证码列表</h1>
+        <v-data-table :items="listCaptchaResp.captchaList" :headers="captchaTableHeaders">
+          <template v-slot:item.captchaType="{ item }">
+            <span v-if="item.captchaType===CaptchaType.PIC_CAPTCHA">图片</span>
+            <span v-if="item.captchaType===CaptchaType.SLIDER_CAPTCHA">滑动</span>
+            <span v-if="item.captchaType===CaptchaType.UNSAFE_DEVICE_LOGIN_VERIFY">设备锁</span>
+          </template>
+          <template v-slot:item.captchaType="{ item }">
+            <span v-if="item.captchaType===CaptchaType.PIC_CAPTCHA">图片</span>
+            <span v-if="item.captchaType===CaptchaType.SLIDER_CAPTCHA">滑动</span>
+            <span v-if="item.captchaType===CaptchaType.UNSAFE_DEVICE_LOGIN_VERIFY">设备锁</span>
+            <span v-if="item.captchaType===CaptchaType.SMS">短信</span>
+          </template>
+          <template v-slot:item.detail="{ item }">
+            <img v-if="item.captchaType===CaptchaType.PIC_CAPTCHA" :src="getImage(item.image)"
+                 style="height: 36px"/>
+            <a v-if="item.captchaType===CaptchaType.SLIDER_CAPTCHA" target="_blank" :href="item.url">链接</a>
+            <a v-if="item.captchaType===CaptchaType.UNSAFE_DEVICE_LOGIN_VERIFY" target="_blank"
+               :href="item.url">链接</a>
+            <span v-if="item.captchaType===CaptchaType.SMS">{{ item.url }}</span>
+          </template>
+        </v-data-table>
+
+      </v-container>
+    </v-card>
+
+
+  </v-container>
 </template>
 
 <script lang="ts">
-    // @ is an alias to /src
+// @ is an alias to /src
 
 
-    import {Component, Vue} from "vue-property-decorator";
-    import {dto} from "@/client/gen/proto";
-    import ICreateBotReq = dto.ICreateBotReq;
-    import Long from "long";
-    import {service} from "@/client/RpcImpl";
-    import IListBotResp = dto.IListBotResp;
-    import ListBotReq = dto.ListBotReq;
-    import IListCaptchaResp = dto.IListCaptchaResp;
-    import ListCaptchaReq = dto.ListCaptchaReq;
-    import CaptchaType = dto.Captcha.CaptchaType;
+import {Component, Vue} from "vue-property-decorator";
+import {dto} from "@/client/gen/proto";
+import Long from "long";
+import {service} from "@/client/RpcImpl";
+import ICreateBotReq = dto.ICreateBotReq;
+import IListBotResp = dto.IListBotResp;
+import ListBotReq = dto.ListBotReq;
+import IListCaptchaResp = dto.IListCaptchaResp;
+import ListCaptchaReq = dto.ListCaptchaReq;
+import CaptchaType = dto.Captcha.CaptchaType;
+import IQRCodeLoginResp = dto.IQRCodeLoginResp;
+import QRCodeLoginState = dto.QRCodeLoginResp.QRCodeLoginState;
 
-    @Component
-    export default class Home extends Vue {
-        CaptchaType = CaptchaType
-
-        mounted() {
-            setInterval(() => {
-                this.listBot()
-                this.listCaptcha()
-            }, 1000)
+@Component
+export default class Home extends Vue {
+  mounted() {
+    setInterval(async () => {
+      this.listBotResp = await service.listBot(new ListBotReq({}))
+      this.listCaptchaResp = await service.listCaptcha(new ListCaptchaReq())
+      if (this.fetchQRCodeResp && !!this.fetchQRCodeResp.sig) {
+        this.queryQRCodeResp = await service.queryQRCodeStatus({sig: this.fetchQRCodeResp.sig})
+        if (this.queryQRCodeResp.state === QRCodeLoginState.QRCodeConfirmed) {
+          this.fetchQRCodeResp = {} // 停止获取
         }
+      }
+    }, 5000)
+  }
 
-        createBotId: string = ''
-        createBotPassword: string = ''
+  showCreateDialog: boolean = false
 
-        get createBotReq(): ICreateBotReq {
-            return {
-                botId: Long.fromString(this.createBotId),
-                password: this.createBotPassword
-            }
-        }
 
-        createBot() {
-            service.createBot(this.createBotReq).then(resp => {
-                alert("创建成功，请处理下方验证码，如果是设备锁验证码填1234")
-            }).catch(reason => {
-                alert("创建失败")
-            })
-        }
+  fetchQRCodeResp: IQRCodeLoginResp = {}
+  queryQRCodeResp: IQRCodeLoginResp = {}
 
-        getImage(bytes: Uint8Array): string {
-            let data = ""
-            for (let i = 0; i < bytes.length; i++) {
-                data += String.fromCharCode(bytes[i])
-            }
-            return "data:image/png;base64," + window.btoa(data)
-        }
+  CaptchaType = CaptchaType
 
-        listBotResp: IListBotResp = {
-            botList: []
-        }
-        botTableHeaders = [
-            {
-                text: "QQ",
-                value: "botId",
-                align: "center"
-            },
-            {
-                text: "在线",
-                value: "isOnline",
-                align: "center"
-            },
-            {
-                text: "登陆",
-                value: "login",
-                align: "center"
-            }
-        ]
+  createBotId: string = ''
+  createBotPassword: string = ''
 
-        async listBot() {
-            this.listBotResp = await service.listBot(new ListBotReq({}))
-        }
+  async onFetchQRCodeClick() {
+    this.fetchQRCodeResp = await service.fetchQRCode({})
+  }
 
-        login(botId: Long) {
-            service.botLoginAsync({
-                botId: botId
-            })
-        }
-
-        listCaptchaResp: IListCaptchaResp = {
-            captchaList: []
-        }
-
-        captchaTableHeaders = [
-            {
-                text: "QQ",
-                value: "botId",
-                align: "center"
-            },
-            {
-                text: "验证类型",
-                value: "captchaType",
-                align: "center"
-            },
-            {
-                text: "详情",
-                value: "detail",
-                align: "center"
-            }
-        ]
-
-        async listCaptcha() {
-            this.listCaptchaResp = await service.listCaptcha(new ListCaptchaReq())
-        }
-
-        captchaBotId: string = ''
-        captchaResult: string = ''
-
-        solveCaptcha() {
-            service.solveCaptcha({
-                botId: Long.fromString(this.captchaBotId),
-                result: this.captchaResult
-            })
-        }
-
+  get createBotReq(): ICreateBotReq {
+    return {
+      botId: Long.fromString(this.createBotId),
+      password: this.createBotPassword
     }
+  }
+
+  createBot() {
+    service.createBot(this.createBotReq).then(resp => {
+      alert("创建成功，请处理下方验证码，如果是设备锁验证码填1234")
+    }).catch(reason => {
+      alert("创建失败")
+    })
+  }
+
+  getImage(bytes: Uint8Array): string {
+    let data = ""
+    for (let i = 0; i < bytes.length; i++) {
+      data += String.fromCharCode(bytes[i])
+    }
+    return "data:image/png;base64," + window.btoa(data)
+  }
+
+  listBotResp: IListBotResp = {
+    botList: []
+  }
+  botTableHeaders = [
+    {
+      text: "QQ",
+      value: "botId",
+      align: "center"
+    },
+    {
+      text: "在线",
+      value: "isOnline",
+      align: "center"
+    },
+    {
+      text: "登陆",
+      value: "login",
+      align: "center"
+    }
+  ]
+
+  login(botId: Long) {
+    service.botLoginAsync({
+      botId: botId
+    })
+  }
+
+  listCaptchaResp: IListCaptchaResp = {
+    captchaList: []
+  }
+
+  captchaTableHeaders = [
+    {
+      text: "QQ",
+      value: "botId",
+      align: "center"
+    },
+    {
+      text: "验证类型",
+      value: "captchaType",
+      align: "center"
+    },
+    {
+      text: "详情",
+      value: "detail",
+      align: "center"
+    }
+  ]
+
+
+  captchaBotId: string = ''
+  captchaResult: string = ''
+
+  solveCaptcha() {
+    service.solveCaptcha({
+      botId: Long.fromString(this.captchaBotId),
+      result: this.captchaResult
+    })
+  }
+
+}
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  devServer: {
+    port: 9001,
+    proxy: 'http://localhost:9000/'
+  },
   "transpileDependencies": [
     "vuetify"
   ]
